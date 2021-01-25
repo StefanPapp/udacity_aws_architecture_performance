35f44d3a596e19f96d749138d58382d5bd0ddbd4
Stefan Papp <stefan.papp@data-wizard.net>
2021-01-25 22:43:46 +0400

    chore: ignoring some terraform meta files

--- task5/Exercise_2/outputs.tf
+++ task5/Exercise_2/outputs.tf
@@ -1,1 +1,4 @@
 # TODO: Define the output variable for the lambda function.
+output "instance_ip_addr" {
+  value = aws_instance.server.private_ip
+}
--- task5/Exercise_2/variables.tf
+++ task5/Exercise_2/variables.tf
@@ -1,1 +1,6 @@
 # TODO: Define the variable for aws_region
+variable region {
+  description = "AWS region in which to create resources"
+  type        = string
+  default     = "eu-central-1"
+}
